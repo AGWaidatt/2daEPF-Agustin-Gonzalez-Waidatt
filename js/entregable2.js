3cebf2606621cb428fb8f128f36c722039dee6ba
Agustin Gonzalez Waidatt <aguswai@hotmail.com>
2022-09-23 23:25:30 -0300

    entrega final (v3)

--- js/entregable2.js
+++ js/entregable2.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         console.log("apreto Enter");
@@ -7,6 +6,7 @@ document.addEventListener("keyup", function(event) {
 
 const precioCuota = 3000
 let cuotasAgregadas = document.querySelector("#cuotaAgregada")
+let dataAgregada = document.querySelector("#dataAgregada")
 
 
 function añadirCuota(){
@@ -40,5 +40,44 @@ borrar = (lista)=>{
 }
 
 
+let form = document.getElementById("form")
+
+function enviarDato(){
+    localStorage.setItem("formulario", JSON.stringify(document.formulario.dato.value));
+}
+
+
+function recuperarDato(){
+    JSON.parse(localStorage.getItem("formulario"))
+    console.log(JSON.parse(localStorage.getItem("formulario")));
+}
+
+
+
+
+function getData(){
+    document.querySelector(".getApiData")
+    console.log("hola")
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(response => response.json())
+    .then(json => mostrarDataAPI(json))
+}
+
+
+
+
+function mostrarDataAPI(dataApi){
+    let cantidadCuotas = parseInt(prompt("Ingrese numero de cuotas"))
+    let lista = document.createElement("div")
+
+    dataApi.forEach(element => {
+        let data = document.createElement("li")
+        data.innerHTML = `${element.title}`
+        
+        lista.appendChild(data)
+    });
+    dataAgregada.appendChild(lista)
+
+}
 
  
